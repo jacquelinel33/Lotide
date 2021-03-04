83d622f4c41b7caf120f06d790d2b48def639700
Jacqueline Lee <jlee4332@gmail.com>
2021-03-04 13:39:05 -0500

    ran eslint

--- eqObjects.js
+++ eqObjects.js
@@ -3,7 +3,7 @@
 //check if objects have same number of keys
 //check if value of one key is the same value in the other object
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😀😀😀😀Asssertion Passed: ${actual} === ${expected}`);
   } else {
@@ -11,11 +11,11 @@ const assertEqual = function (actual, expected) {
   }
 };
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    for (i = 0; i < array1.length; i++) {
+    for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
         return false;
       }
@@ -24,7 +24,7 @@ const eqArrays = function (array1, array2) {
 };
 
 
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
   //checks if key arrays is same length. Ends is false. Continue checking if object:key exists
@@ -46,13 +46,13 @@ const eqObjects = function (object1, object2) {
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
-assertEqual(eqObjects(ab, ba), true)
+assertEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc); // => false
-assertEqual(eqObjects(ab, abc), false)
+assertEqual(eqObjects(ab, abc), false);
 
 
 const cd = { c: "1", d: ["2", 3, 3, 6, 2, 1] };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false)
+assertEqual(eqObjects(cd, cd2), false);
