7f45068b31f78c88ad569a682d4e04eb5090273b
Jacqueline Lee <jlee4332@gmail.com>
2021-03-04 13:36:47 -0500

    run eslint

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,4 +1,4 @@
-const eqObjects = function (object1, object2) {
+const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
   //checks if key arrays is same length. Ends is false. Continue checking if object:key exists
@@ -15,12 +15,12 @@ const eqObjects = function (object1, object2) {
   } return true;
 };
 
-const eqArrays = function (array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    for (i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) 
         return false;
       }
     } return true;
@@ -29,9 +29,9 @@ const eqArrays = function (array1, array2) {
 
 const assertObjectsEqual = function(actual, expected) {
   if (eqObjects(actual, expected) === true) {
-    console.log(`😀😀😀😀 Assertion passed! ${actual} === ${expected}`)
+    console.log(`😀😀😀😀 Assertion passed! ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion failed! ${actual} !== ${expected}`)
+    console.log(`Assertion failed! ${actual} !== ${expected}`);
   }
 };
 
@@ -40,13 +40,13 @@ const assertObjectsEqual = function(actual, expected) {
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
-assertObjectsEqual(eqObjects(ab, ba), true)
+assertObjectsEqual(eqObjects(ab, ba), true);
 
 const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc); // => false
-assertObjectsEqual(eqObjects(ab, abc), false)
+assertObjectsEqual(eqObjects(ab, abc), false);
 
 
 const cd = { c: "1", d: ["2", 3, 3, 6, 2, 1] };
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertObjectsEqual(eqObjects(cd, cd2), false)
+assertObjectsEqual(eqObjects(cd, cd2), false);
