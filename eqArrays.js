3703b4c4f4607a787538c0d426a4db558bee47f0
Jacqueline <jlee4332@gmail.com>
2021-03-08 14:13:31 -0500

    refactor eqArrays.js with exports and test file

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   } else {
-    for (let i = 0; i < array1.length; i++) {
+    for (let i in array1) {
       if (array1[i] !== array2[i]) {
         return false;
       }
@@ -10,8 +10,4 @@ const eqArrays = function(array1, array2) {
   }
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3,3,5,2])); // => false
+module.exports = eqArrays;
