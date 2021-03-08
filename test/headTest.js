58b1321c715eadd68a02473cd4eb558f726cf64a
Jacqueline <jlee4332@gmail.com>
2021-03-08 15:04:31 -0500

    refactor head.js  and adding test

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 2 for [2,3,4,6,67]", () => {
+    assert.strictEqual(head([2,3,4,6,67]), 2);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+});
+
