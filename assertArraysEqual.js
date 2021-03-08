c80ae7eb7e790284f8b486165d9c95b707d2d8d0
Jacqueline <jlee4332@gmail.com>
2021-03-08 14:44:56 -0500

    adding middle.js refactor and test file

---
+++ assertArraysEqual.js
@@ -0,0 +1,11 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(exptectedArr, actualArr) {
+  if (eqArrays(exptectedArr,actualArr) === true) {
+    console.log(`😀😀😀😀 Assertion passed! ${exptectedArr} === ${actualArr}`);
+  } else {
+    console.log(`Assertion failed! ${exptectedArr} !== ${actualArr}`);
+  }
+};
+
+module.exports = assertArraysEqual; 
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+let middle = require('../middle');
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
