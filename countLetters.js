eb3b5e21cf7771616bc58494c0a5159a15c4f629
Jacqueline <jlee4332@gmail.com>
2021-10-27 10:20:07 -0700

    refactor

--- countLetters.js
+++ countLetters.js
@@ -2,31 +2,17 @@ const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = require('./assertArraysEqual');
 
-// const countLetters = function (stringInput) {
-//   let result = {};
-
-//   for (let letter of stringInput) {
-//     if (!result[letter]) {
-//       result[letter] = 0;
-//     }
-//     result[letter]++;
-//   }
-//   delete result[' '];
-//   return result;
-// };
-
-
 const countLetters = (input) => {
-  let result = {}
-  for(let letter of input) {
-    if (result[letter]){
+  let result = {};
+  for (let letter of input.toUpperCase()) {
+    if (result[letter]) {
       result[letter]++;
     } else {
-      result[letter] =1;
+      result[letter] = 1;
     }
   }
   return result;
-}
+};
 
 console.log(countLetters('LHL'));
 module.exports = countLetters;
